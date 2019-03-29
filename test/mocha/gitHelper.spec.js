c9a42fbc446d07a066f3678988abd3d693e87e7b
tallyb <tally.barak@gmail.com>
2019-03-29 16:50:54 +0300

    chore(app): Rewrite git helper with await

--- test/mocha/gitHelper.spec.js
+++ test/mocha/gitHelper.spec.js
@@ -6,13 +6,8 @@ var _ = require('lodash');
 
 describe('gitHelper', () => {
     describe('getCurrentBranch()', () => {
-        it('should return current branch', (done) => {
-            gitHelper.getCurrentBranch()
-                .then(branch => {
-                    expect(branch).to.not.be.null;
-                    done();
-                })
-                .catch(done);
+        it('should return current branch', async () => {
+            expect( await gitHelper.getCurrentBranch()).to.not.be.null;
         });
     });
 
@@ -72,40 +67,26 @@ describe('gitHelper', () => {
     });
 
     describe('getRemoteRepository()', () => {
-        it('should return remote origin repository', (done) => {
-
-            gitHelper.getRemoteRepository()
-                .then(remote => {
-                    expect(remote.url).to.not.be.null;
-                    expect(remote.repo).to.not.be.null;
-                    expect(remote.owner).to.not.be.null;
-                    expect(_.startsWith(remote.url, 'https://')).to.be.true;
-                    done();
-                })
-                .catch(done);
+        it('should return remote origin repository', async () => {
+            let remote = await gitHelper.getRemoteRepository();
+            expect(remote.url).to.not.be.null;
+            expect(remote.repo).to.not.be.null;
+            expect(remote.owner).to.not.be.null;
+            expect(_.startsWith(remote.url, 'https://')).to.be.true;
         });
 
-        it('should throw error with an unknow remote', (done) => {
-            gitHelper.getRemoteRepository('dummy')
-                .then(remote => {
-                    done(new Error('should throw an error'));
-                })
-                .catch(err => {
-                    expect(err).not.to.be.null;
-                    done();
-                });
+        it('should throw error with an unknow remote', async () => {
+            try {
+                await gitHelper.getRemoteRepository('dummy');
+            } catch (err) {
+                expect(err).not.to.be.null;
+            }
         });
     });
 
     describe('isClean()', () => {
-        it('should return a boolean', (done) => {
-
-            gitHelper.isClean()
-                .then(res => {
-                    expect(res).to.be.a('boolean');
-                    done();
-                })
-                .catch(done);
+        it('should return a boolean', async () => {
+            expect(await gitHelper.isClean()).to.be.a('boolean');
         });
     });
 });
